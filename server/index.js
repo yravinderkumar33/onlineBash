11fda6ca949c596fc3e60ad905959f8a0ff60a83
yravinderkumar33 <yravinderkumar33@gmail.com>
2018-11-20 13:35:59 +0530

    minor changes inside server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const {spawn , exec} = require('child_process');
+const { spawn, exec } = require('child_process');
 
 app.listen(4000, () => {
     console.log("listening on port 4000");
@@ -16,18 +16,14 @@ app.use(function (req, res, next) {
     next();
 });
 
-
 app.post("/api/commands", (req, res) => {
     const body = req.body.body;
     const header = "#!/bin/bash \n";
-    console.log(header+body);
-    fs.writeFileSync("./script.sh", header+body);
-    exec("chmod 777 script.sh && ./script.sh" , (error,stdout , stderr)=>{
-        
-        if(error) res.status(404).json({error:"forbidden"})
-
-        console.log(stdout);
-        res.json({ result : stdout })
+    console.log(header + body);
+    fs.writeFileSync("./script.sh", header + body);
+    exec("chmod 777 script.sh && ./script.sh", (error, stdout, stderr) => {
+        if (error) res.json({ result: "please enter a valid command" })
+        if (stderr) res.json({ result: "please enter a valid command" })
+        res.json({ result: stdout })
     })
-
 })
--- server/script.sh
+++ server/script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-ifconfig
+mkdir a
